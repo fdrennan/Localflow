ec0fda7b053cae1ead76531c89bfe95d79c357af
Freddy Drennan <drennanfreddy@gmail.com>
2020-04-29 06:34:26 -0600

    update

--- dags/refresh_mataterialized_views.py
+++ dags/refresh_mataterialized_views.py
@@ -12,7 +12,7 @@ args = {
 }
 dag = DAG(dag_id='refresh_materialized_views',
           default_args=args,
-          schedule_interval='* * * * *',
+          schedule_interval='*/15 * * * *',
           concurrency=1,
           max_active_runs=1,
           catchup=False)
