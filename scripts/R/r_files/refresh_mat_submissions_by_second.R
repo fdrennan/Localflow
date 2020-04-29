c3270dd942b74cb3e9ded5dbbafbe3ebb6239759
Freddy Drennan <drennanfreddy@gmail.com>
2020-04-28 23:17:10 -0600

    update

--- scripts/R/r_files/refresh_mat_submissions_by_second.R
+++ scripts/R/r_files/refresh_mat_submissions_by_second.R
@@ -5,8 +5,8 @@ refresh <- function() {
   con = postgres_connector()
   on.exit(dbDisconnect(con))
   dbExecute(
-    conn = con, 
-    statement = read_file('../../sql/materialized_views/mat_submissions_by_second.sql')
+    conn = con,
+    statement = read_file('../../sql/materialized_views/refresh_mat_submissions_by_second.sql')
   )
 }
 
